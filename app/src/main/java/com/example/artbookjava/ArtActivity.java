78c559a9fd57eb51e548358e03c21973adc557c4
Turgay Uludağ <uludagturgay32@gmail.com>
2025-02-19 12:15:09 +0300

    SQLİTE kayıt

--- app/src/main/java/com/example/artbookjava/ArtActivity.java
+++ app/src/main/java/com/example/artbookjava/ArtActivity.java
@@ -11,6 +11,8 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteStatement;
 import android.graphics.Bitmap;
 import android.graphics.ImageDecoder;
 import android.net.Uri;
@@ -23,11 +25,15 @@ import android.widget.Toast;
 import com.example.artbookjava.databinding.ActivityArtBinding;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.io.ByteArrayOutputStream;
+
 public class ArtActivity extends AppCompatActivity {
 private ActivityArtBinding binding;
 ActivityResultLauncher<Intent> activityResultLauncher;
 ActivityResultLauncher<String > permissionLauncher;
     Bitmap selectedImage;
+
+    SQLiteDatabase database;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +47,60 @@ ActivityResultLauncher<String > permissionLauncher;
 
     public void save(View view){
 
+        String name=binding.nameText.getText().toString();
+        String artisitName=binding.artistText.getText().toString();
+        String year=binding.yearText.getText().toString();
+
+            Bitmap smallImage=makeSmallerImage(selectedImage,300);
+
+        ByteArrayOutputStream outputStream=new ByteArrayOutputStream();
+        smallImage.compress(Bitmap.CompressFormat.PNG,50,outputStream);
+        byte[] byteArray=outputStream.toByteArray();
+
+        try {
+            database=this.openOrCreateDatabase("Arts",MODE_PRIVATE,null);
+            database.execSQL("CREATE TABLE IF NOT EXISTS arts(id INTEGER PRIMARY KEY,artname VARCHAR, paintername VARCHAR, year VARCHAR, image BLOB)");
+
+            String sqlString="INSERT INTO arts(artname, paintername, year, image) VALUES(?,?,?,?)";
+            SQLiteStatement sqLiteStatement=database.compileStatement(sqlString);
+            sqLiteStatement.bindString(1,name);
+            sqLiteStatement.bindString(2,artisitName);
+            sqLiteStatement.bindString(3,year);
+            sqLiteStatement.bindBlob(4,byteArray);
+            sqLiteStatement.execute();
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        Intent intent=new Intent(ArtActivity.this,MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+
     }
+
+    public Bitmap makeSmallerImage(Bitmap image,int maximumSize){
+        int width=image.getWidth();
+        int height=image.getHeight();
+
+        float bitmapRatio=(float) width/(float) height;
+
+        if (bitmapRatio>1){
+            //landscape image
+            width =maximumSize;
+            height=(int) (width/bitmapRatio);
+        }else {
+            //portmait image
+            height=maximumSize;
+            width=(int) (height*bitmapRatio);
+        }
+
+
+        return image.createScaledBitmap(image,width,height,true);
+    }
+
+
     public void selectImage(View view){
         if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.TIRAMISU){
             //Android 33+ --> READ_MEDİA_IMAGES
