99422838ec6b7541f7fd4779068f7aee5a0080be
Turgay Uludağ <uludagturgay32@gmail.com>
2025-02-21 11:17:20 +0300

    ArtbookJava

--- app/src/main/java/com/example/artbookjava/MainActivity.java
+++ app/src/main/java/com/example/artbookjava/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.artbookjava;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -12,9 +15,13 @@ import android.view.View;
 
 import com.example.artbookjava.databinding.ActivityMainBinding;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    ArrayList<Art> artArrayList;
+    ArtAdapter artAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,38 @@ public class MainActivity extends AppCompatActivity {
         View view =binding.getRoot();
         setContentView(view);
 
+        artArrayList =new ArrayList<>();
+
+        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+            artAdapter=new ArtAdapter(artArrayList);
+            binding.recyclerView.setAdapter(artAdapter);
+
+            getData();
+    }
+
+    private void getData(){
+        try {
+            SQLiteDatabase sqLiteDatabase=this.openOrCreateDatabase("Arts",MODE_PRIVATE,null);
+
+            Cursor cursor=sqLiteDatabase.rawQuery("SELECT * FROM arts",null);
+
+            int nameIx=cursor.getColumnIndex("artname");
+            int idIx=cursor.getColumnIndex("id");
+
+            while (cursor.moveToNext()){
+                String name= cursor.getString(nameIx);
+                int id=cursor.getInt(idIx);
+                Art art=new Art(name,id);
+                artArrayList.add(art);
+
+
+            }
+            artAdapter.notifyDataSetChanged();
+            cursor.close();
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -41,6 +80,7 @@ public class MainActivity extends AppCompatActivity {
 
         if (item.getItemId()==R.id.add_art){
             Intent intent=new Intent(this,ArtActivity.class);
+            intent.putExtra("info","new");
             startActivity(intent);
         }
 
